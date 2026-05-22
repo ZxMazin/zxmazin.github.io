ef9b6043c27e670d4b2b6e35a5b07b0fa0eb7611
Mazin <elmelmaz@icloud.com>
2026-05-22 07:18:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -379,7 +379,7 @@ function showStartVideo() {
     interlude.innerHTML = `
         <div class="interlude-overlay">
             <h2>Interlude Musical...</h2>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/HJuGB3M2Lu8?si=Q0EjcU118FDkEQkH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/HJuGB3M2Lu8?si=WuLjPa66qB0nMqF6&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <button class="retro-btn">
                 ${s.skip_interlude}
             </button>
